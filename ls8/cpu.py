937b6163e45e8598df6e06ed2634b6c88f674e53
Darragh Ferry <darraghferry@hotmail.com>
2020-05-19 17:11:18 +0100

    day2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,26 +12,35 @@ class CPU:
         self.ram = [0] * 255
         self.reg = [0] * 8
 
-    def load(self):
+    def load(self, file_load):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        try:
+            with open(f"examples/{file_load}") as f:
+                for line in f:
+                    num = line.split("#" or "\n")[0].replace('\n', '')
+                    self.ram[address] = int(num, 2)
+                    address += 1
+        except FileNotFoundError:
+            print("file not found")
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -39,6 +48,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == "MULTIPLY":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,20 +81,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        MUL = 0b10100010
+
         run = True
         command = self.ram[self.pc]
         while run:
-            if command == 0b10000010:
+            if command == LDI:
                 # load value into register
                 reg_index = self.ram[self.pc + 1]
                 num = self.ram[self.pc + 2]
                 self.reg[reg_index] = num
                 self.pc += 3
-            elif command == 0b01000111:
+            elif command == MUL:
+                reg_index1 = self.ram[self.pc + 1]
+                reg_index2 = self.ram[self.pc + 2]
+                self.alu("MULTIPLY", reg_index1, reg_index2)
+                self.pc += 3
+            elif command == PRN:
                 reg_index = self.ram[self.pc + 1]
                 print(self.reg[reg_index])
                 self.pc += 2
-            elif command == 0b00000001:
+            elif command == HLT:
                 run = False
             else:
                 print(f"invalid instruction [{self.ram[self.pc]}]")
